feb99552da7ce341116e02b071497ac7b3d7a700
mustafa-farag <mustafafarag978@gmail.com>
2022-07-19 22:21:05 +0200

    [Character details screen] Sliver List

--- lib/presentation/screens/details_screen.dart
+++ lib/presentation/screens/details_screen.dart
@@ -26,13 +26,81 @@ class DetailsScreen extends StatelessWidget {
     );
   }
 
+  Widget _buildCharacterInfo(String title, String value) {
+    return RichText(
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      text: TextSpan(
+        children: [
+          TextSpan(
+              text: title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              )),
+          TextSpan(
+              text: value,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              )),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDivider(double endIndent) {
+    return Divider(
+      height: 25,
+      endIndent: endIndent,
+      color: MyColors.appBarColor,
+      thickness: 2,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.backgroundColor,
       body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
         slivers: [
           _buildSliverAppBar(),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildCharacterInfo('Job : ', character.jobs.join(" / ")),
+                  _buildDivider(290),
+                  _buildCharacterInfo(
+                      'Appeared in : ', character.categoryForTwoSeries),
+                  _buildDivider(220),
+                  _buildCharacterInfo(
+                      'Status : ', character.statusIfDeadOrAlive),
+                  _buildDivider(265),
+                  if (character.appearanceOfSeasons.isNotEmpty)
+                    _buildCharacterInfo('Seasons : ',
+                        character.appearanceOfSeasons.join(" / ")),
+                  if (character.appearanceOfSeasons.isNotEmpty)
+                    _buildDivider(250),
+                  if (character.betterCallSaulAppearance.isNotEmpty)
+                    _buildCharacterInfo('Better Call Saul Seasons : ',
+                        character.betterCallSaulAppearance.join(" / ")),
+                  if (character.betterCallSaulAppearance.isNotEmpty)
+                    _buildDivider(120),
+                  _buildCharacterInfo('Actor/Actress : ', character.actorName),
+                  _buildDivider(210),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 400,
+            )
+          ])),
         ],
       ),
     );
